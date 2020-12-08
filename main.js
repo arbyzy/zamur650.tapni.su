333356aaa2d3224c4dd196e212c050ac16b62b3f
Zamur650 <57483029+Zamur650@users.noreply.github.com>
2020-12-08 21:54:35 +0700

    :)

--- main.js
+++ main.js
@@ -16,17 +16,17 @@ var vue = new Vue({
 		goToZm() {
 			location.href = 'index.html';
 		},
+
 		goToPd() {
 			location.href = 'pokedex/index.html';
 		},
+
 		goToVkBuy() {
 			location.href = 'https://vk.com/rubin_alexey';
 		},
+		
 		goToVkSay() {
 			location.href = 'https://vk.com/im?sel=550222194';
-		},
-		goToDiscord() {
-			location.href = 'https://discord.gg/QEwp97z';
 		}
 	}
 })
